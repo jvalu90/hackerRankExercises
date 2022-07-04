9505e18e14dbe75f72f0978d3774896765a28de5
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-04 02:03:44 +0000

    July 3 - Diagonal Difference

--- index.js
+++ index.js
@@ -34,4 +34,65 @@ function aVeryBigSum(ar) {
   return ar.reduce((total, element) => total + element, 0)
 }
 
-log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])) // 5000000015
+//log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])) // 5000000015
+
+/*
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix
+
+is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+
+The left-to-right diagonal =
+. The right to left diagonal = . Their absolute difference is
+
+.
+
+Function description
+
+Complete the
+
+function in the editor below.
+
+diagonalDifference takes the following parameter:
+
+    int arr[n][m]: an array of integers
+
+Return
+
+    int: the absolute diagonal difference
+
+Input Format
+
+The first line contains a single integer,
+, the number of rows and columns in the square matrix .
+Each of the next lines describes a row, , and consists of space-separated integers
+
+.
+
+Constraints
+
+Output Format
+
+Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+*/
+
+function diagonalDifference(arr) {
+  let leftToRightSum = 0;
+  let rightToLeftSum = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    leftToRightSum += arr[i][i]
+    rightToLeftSum += arr[i][arr.length - i - 1]
+  }
+
+  return Math.abs(leftToRightSum - rightToLeftSum)
+}
+
+log(diagonalDifference([[11, 2, 4], 
+                        [4, 5, 6], 
+                        [10, 8, -12]])) // 15
