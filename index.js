d39c7b6320a957e007fceea0bc94aaa4ae5429e2
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-06 01:54:03 +0000

    July 5 - Plus Minus

--- index.js
+++ index.js
@@ -93,6 +93,84 @@ function diagonalDifference(arr) {
   return Math.abs(leftToRightSum - rightToLeftSum)
 }
 
-log(diagonalDifference([[11, 2, 4], 
+/*log(diagonalDifference([[11, 2, 4], 
                         [4, 5, 6], 
-                        [10, 8, -12]])) // 15
+                        [10, 8, -12]])) // 15 */
+
+/*
+Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with
+
+places after the decimal.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to
+
+are acceptable.
+
+Example
+There are elements, two positive, two negative and one zero. Their ratios are , and
+
+. Results are printed as:
+
+0.400000
+0.400000
+0.200000
+
+Function Description
+
+Complete the plusMinus function in the editor below.
+
+plusMinus has the following parameter(s):
+
+    int arr[n]: an array of integers
+
+Print
+Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with
+
+digits after the decimal. The function should not return a value.
+
+Input Format
+
+The first line contains an integer,
+, the size of the array.
+The second line contains space-separated integers that describe
+
+.
+
+Constraints
+
+
+Output Format
+
+Print the following
+lines, each to
+
+decimals:
+
+    proportion of positive values
+    proportion of negative values
+    proportion of zeros
+
+*/
+
+function plusMinus(arr) {
+  let positiveElements = 0
+  let negativeElements = 0
+  let ceroElements = 0
+  for(let element of arr) {
+    if(element > 0) {
+      positiveElements++
+    } else if (element < 0) {
+      negativeElements++
+    } else {
+      ceroElements++
+    }
+  }
+
+  console.log((positiveElements/arr.length).toFixed(6))
+  console.log((negativeElements/arr.length).toFixed(6))
+  console.log((ceroElements/arr.length).toFixed(6))
+}
+
+log(plusMinus([-4, 3, -9, 0, 4, 1]))  // 0.500000
+                                      // 0.333333
+                                      //  0.166667
