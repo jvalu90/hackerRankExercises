5acaf640b4d1dea5c23b43154f6e46ebe9d1c883
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-11 00:15:58 +0000

    July 10 - Birthday Cake Candles

--- index.js
+++ index.js
@@ -351,4 +351,51 @@ function miniMaxSum(arr) {
   console.log(min, max)
 }
 
-miniMaxSum([1, 2, 3, 4, 5]) // 10 14
+//miniMaxSum([1, 2, 3, 4, 5]) // 10 14
+
+/*
+Birthday Cake Candles
+
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Example
+
+The maximum height candles are units high. There are of them, so return
+
+.
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+
+birthdayCakeCandles has the following parameter(s):
+
+    int candles[n]: the candle heights
+
+Returns
+
+    int: the number of candles that are tallest
+
+Input Format
+
+The first line contains a single integer,
+, the size of .
+The second line contains space-separated integers, where each integer describes the height of
+
+.
+
+Constraints
+*/
+
+function birthdayCakeCandles(candles) {
+  let counter = 0;
+  const maxCandle = Math.max(...candles);
+
+  candles.forEach(element => {
+    if(element === maxCandle) counter++
+  })
+
+  return counter
+}
+
+log(birthdayCakeCandles([3, 2, 1, 3])) //2
