d759bb9433bdcf9b2951ef46f0e8f31d5db9d46b
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-20 23:39:07 +0000

    July 20 - Highest Value Palindrome

--- index.js
+++ index.js
@@ -537,8 +537,80 @@ function isValid(s) {
   return response
 }
 
-log(isValid('aabbccddeefghi')) // NO
-log(isValid('abcdefghhgfedecba')) // YES
-log(isValid('aabbc')) // YES
-log(isValid('ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd')) // YES
-log(isValid('xxxaabbccrry')) // NO
+//log(isValid('aabbccddeefghi')) // NO
+//log(isValid('abcdefghhgfedecba')) // YES
+//log(isValid('aabbc')) // YES
+//log(isValid('ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd')) // YES
+//log(isValid('xxxaabbccrry')) // NO
+
+/*
+Palindromes are strings that read the same from the left or right, for example madam or 0110.
+
+You will be given a string representation of a number and a maximum number of changes you can make. Alter the string, one digit at a time, to create the string representation of the largest number possible given the limit to the number of changes. The length of the string may not be altered, so you must consider
+'s left of all higher digits in your tests. For example is valid,
+
+is not.
+
+Given a string representing the starting number, and a maximum number of changes allowed, create the largest palindromic string of digits possible or the string '-1' if it is not possible to create a palindrome under the contstraints.
+
+Example
+
+Make replacements to get
+
+.
+
+
+Make replacement to get
+
+.
+
+Function Description
+
+Complete the highestValuePalindrome function in the editor below.
+
+highestValuePalindrome has the following parameter(s):
+
+    string s: a string representation of an integer
+    int n: the length of the integer string
+    int k: the maximum number of changes allowed
+
+Returns
+
+    string: a string representation of the highest value achievable or -1
+
+Input Format
+
+The first line contains two space-separated integers,
+and , the number of digits in the number and the maximum number of changes allowed.
+The second line contains an
+
+-digit string of numbers.
+
+Constraints
+
+Each character in the number is an integer where .
+*/
+
+function highestValuePalindrome(s, n, k) {
+  s = s.split('');
+  let isPalindrome
+  
+  for (let i = 0; i < Math.trunc(n/2) ; i++) {
+    if ((s[i] !== s[s.length - 1 - i]) && k > 0 ) {
+      s[s.length - 1 - i] = s[i];
+      k--
+    }
+  }
+
+  if (s.join('') === s.reverse().join('')) {
+    isPalindrome = s.join('')
+  } else {
+    isPalindrome = -1
+  }
+  
+  return isPalindrome
+}
+
+log(highestValuePalindrome('3943', 4, 1)) // 3993
+log(highestValuePalindrome('092282', 6, 3)) // 992299
+log(highestValuePalindrome('0011', 4, 1)) // -1
