e42a2ce253504cc4d0a151cd0a1e9db6e564d225
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-23 23:07:37 +0000

    July 23 - Grading Students

--- index.js
+++ index.js
@@ -611,6 +611,81 @@ function highestValuePalindrome(s, n, k) {
   return isPalindrome
 }
 
-log(highestValuePalindrome('3943', 4, 1)) // 3993
-log(highestValuePalindrome('092282', 6, 3)) // 992299
-log(highestValuePalindrome('0011', 4, 1)) // -1
+//log(highestValuePalindrome('3943', 4, 1)) // 3993
+//log(highestValuePalindrome('092282', 6, 3)) // 992299
+//log(highestValuePalindrome('0011', 4, 1)) // -1
+
+/*
+HackerLand University has the following grading policy:
+
+    Every student receives a 
+
+in the inclusive range from to
+.
+Any
+less than
+
+    is a failing grade.
+
+Sam is a professor at the university and likes to round each student's
+
+according to these rules:
+
+    If the difference between the 
+
+and the next multiple of is less than , round up to the next multiple of
+.
+If the value of
+is less than
+
+    , no rounding occurs as the result will still be a failing grade.
+
+Examples
+
+round to
+(85 - 84 is less than 3)
+do not round (result is less than 40)
+
+    do not round (60 - 57 is 3 or higher)
+
+Given the initial value of
+for each of Sam's
+
+students, write code to automate the rounding process.
+
+Function Description
+
+Complete the function gradingStudents in the editor below.
+
+gradingStudents has the following parameter(s):
+
+    int grades[n]: the grades before rounding
+
+Returns
+
+    int[n]: the grades after rounding as appropriate
+
+Input Format
+
+The first line contains a single integer,
+, the number of students.
+Each line of the subsequent lines contains a single integer, .
+*/
+
+function gradingStudents(grades) {
+  return grades.map(element => {
+    if(element < 38) {
+      return element
+    } else {
+      if (3 <= (element % 10) &&  (element % 10) <= 5){
+        return element + ( 5 - element%10)
+      } else if (8 <= (element % 10) && (element % 10) <= 10){
+        return element + (10 - element%10)
+      } else {
+        return element
+      }
+    }
+  })
+}
+
+log(gradingStudents([73, 67, 38, 33])) // [75, 67, 40, 33]
