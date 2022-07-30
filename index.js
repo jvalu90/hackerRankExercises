77e7cff8d91c94c1d2461fe545429e9de63464fd
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-30 20:51:51 +0000

    July 30 - Between Two Sets

--- index.js
+++ index.js
@@ -807,6 +807,58 @@ function kangaroo(x1, v1, x2, v2) {
   }
 }
 
-log(kangaroo(0, 3, 4, 2)) // YES
-log(kangaroo([0, 2, 5, 3])) // NO
+//log(kangaroo(0, 3, 4, 2)) // YES
+//log(kangaroo([0, 2, 5, 3])) // NO
 
+/*
+Between Two Sets
+
+There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+
+    The elements of the first array are all factors of the integer being considered
+    The integer being considered is a factor of all elements of the second array
+
+These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
+Example
+
+There are two numbers between the arrays: and .
+, , and for the first value.
+, and , for the second value. Return
+
+.
+
+Function Description
+
+Complete the getTotalX function in the editor below. It should return the number of integers that are betwen the sets.
+
+getTotalX has the following parameter(s):
+
+    int a[n]: an array of integers
+    int b[m]: an array of integers
+
+Returns
+
+    int: the number of integers that are between the sets
+
+Input Format
+
+The first line contains two space-separated integers,
+and , the number of elements in arrays and .
+The second line contains distinct space-separated integers where .
+The third line contains distinct space-separated integers where .
+*/
+
+function getTotalX(a, b) {
+  let isFactor = (a, b) => a % b === 0;
+
+  let results = []
+  
+  for (let i = a[a.length - 1]; i <= b[0]; i++) {
+    if (a.every( x => isFactor(i, x)) && b.every( x => isFactor(x, i))) results.push(i)
+  }
+  
+  return results.length
+}
+
+log(getTotalX([2, 4], [16, 32, 96]))
