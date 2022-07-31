8b93a25d05b6351a9fbe3ecb8fd0b33e8bb1c6df
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-07-31 23:08:56 +0000

    July 31 - Breaking the Records

--- index.js
+++ index.js
@@ -861,4 +861,66 @@ function getTotalX(a, b) {
   return results.length
 }
 
-log(getTotalX([2, 4], [16, 32, 96]))
+//log(getTotalX([2, 4], [16, 32, 96]))
+
+/*
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
+
+Example
+
+Scores are in the same order as the games played. She tabulates her results as follows:
+
+                                     Count
+    Game  Score  Minimum  Maximum   Min Max
+     0      12     12       12       0   0
+     1      24     12       24       0   1
+     2      10     10       24       1   1
+     3      24     10       24       1   1
+
+Given the scores for a season, determine the number of times Maria breaks her records for most and least points scored during the season.
+
+Function Description
+
+Complete the breakingRecords function in the editor below.
+
+breakingRecords has the following parameter(s):
+
+    int scores[n]: points scored per game
+
+Returns
+
+    int[2]: An array with the numbers of times she broke her records. Index 
+
+is for breaking most points records, and index
+
+    is for breaking least points records.
+
+Input Format
+
+The first line contains an integer
+, the number of games.
+The second line contains space-separated integers describing the respective values of
+
+.
+
+Constraints
+*/
+
+function breakingRecords(scores) {
+  let records = [scores[0], scores[0]], breakBestRecord = 0, breakWrostRecord = 0;
+
+  for (let score of scores)  {
+    if (score > records[0]) {
+      records[0] = score
+      breakBestRecord++
+    } else if (score < records[1]) {
+      records[1] = score
+      breakWrostRecord++
+    }
+  }
+  
+  return [breakBestRecord, breakWrostRecord]  
+}
+
+log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1])) // 2, 4
+log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42])) // 4, 0
