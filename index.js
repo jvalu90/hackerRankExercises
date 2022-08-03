e503f7830ed8b0d043cf86ff19db665520ecc3e7
Jimmy Valencia Urbano <90942748+jvalu90@users.noreply.github.com>
2022-08-03 01:00:47 +0000

    August 02 - Subarray Division

--- index.js
+++ index.js
@@ -922,5 +922,59 @@ function breakingRecords(scores) {
   return [breakBestRecord, breakWrostRecord]  
 }
 
-log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1])) // 2, 4
-log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42])) // 4, 0
+//log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1])) // 2, 4
+//log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42])) // 4, 0
+
+/*
+Two children, Lily and Ron, want to share a chocolate bar. Each of the squares has an integer on it.
+
+Lily decides to share a contiguous segment of the bar selected such that:
+
+    The length of the segment matches Ron's birth month, and,
+    The sum of the integers on the squares is equal to his birth day.
+
+Determine how many ways she can divide the chocolate.
+
+Example
+
+
+Lily wants to find segments summing to Ron's birth day, with a length equalling his birth month, . In this case, there are two segments meeting her criteria: and
+
+.
+
+Function Description
+
+Complete the birthday function in the editor below.
+
+birthday has the following parameter(s):
+
+    int s[n]: the numbers on each of the squares of chocolate
+    int d: Ron's birth day
+    int m: Ron's birth month
+
+Returns
+
+    int: the number of ways the bar can be divided
+
+Input Format
+
+The first line contains an integer
+, the number of squares in the chocolate bar.
+The second line contains space-separated integers , the numbers on the chocolate squares where .
+The third line contains two space-separated integers, and , Ron's birth day and his birth month.
+*/
+
+function birthday(s, d, m) {
+
+  let counter = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.slice(i, (i + m)).reduce((total, value) => total + value, 0) === d) counter ++
+  }
+
+  return counter
+}
+
+log(birthday([1, 2, 1, 3, 2], 3, 2)) // 2
+log(birthday([1, 1, 1, 1, 1, 1], 3, 2)) // 0
+log(birthday([4], 4, 1)) // 1
